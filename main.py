0b1311181a5a05ad5a1ba5f01aa3c20889788308
luckysugandh123-glitch <luckysugandh123@gmail.com>
2026-02-12 14:30:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
 import os
 
-BOT_TOKEN = os.environ.get("8492113943:AAEYiMg0Q4DqiPimyeBA7Qk9JIultTyWDqU")
+BOT_TOKEN = os.environ.get("8492113943:AAGfAo14XXmEdN78W8qed7GwVsZml-jliX8")
 CHAT_ID = os.environ.get("8099868217")
